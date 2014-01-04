06b1ffe2a870f1a1df38eeb2bb9413f43bdfb8cf
Tomoaki Kosugi <kipspro@gmail.com>
2014-01-05 00:01:38 +0900

    Add base exceptions

---
+++ src/NpDocument/Exception/ExceptionInterface.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ *
+ *
+ * @copyright Copyright (c) 2013-2013 KipsProduction (http://www.kips.gr.jp)
+ * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
+ */
+
+namespace NpDocument\Exception;
+
+interface ExceptionInterface {
+}
+
