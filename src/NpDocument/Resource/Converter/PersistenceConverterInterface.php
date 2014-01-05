eb9238515cea1bfb4834cffbed5f904824d98bab
Tomoaki Kosugi <kipspro@gmail.com>
2014-01-05 20:15:46 +0900

    Update 2013-2014

--- src/NpDocument/Resource/Converter/PersistenceConverterInterface.php
+++ src/NpDocument/Resource/Converter/PersistenceConverterInterface.php
@@ -2,7 +2,7 @@
 /*
  *
  *
- * @copyright Copyright (c) 2013-2013 KipsProduction (http://www.kips.gr.jp)
+ * @copyright Copyright (c) 2013-2014 KipsProduction (http://www.kips.gr.jp)
  * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
  */
 namespace NpDocument\Resource\Converter;
